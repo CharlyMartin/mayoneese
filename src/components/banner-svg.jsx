15c239a1f0d8d91982b3507a9d9bacde6d4b6ce9
Charly <charly.martin.1207@gmail.com>
2019-02-28 12:24:58 +0700

    svg on home page working

--- src/components/banner-svg.jsx
+++ src/components/banner-svg.jsx
@@ -1,19 +1,38 @@
+// Librairies
 import React from 'react';
 
-function svgCode(id) {
-  return {
+function BannerSVG({ id, zIndex, top, colour, shade }) {  
+  const style = {
+    zIndex,
+    top
+  } 
+  
+  const svg = {
     __html: `
-      <svg width="1440px" height="829px" viewBox="0 0 1440 829" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-        <g id="${id}" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+      <svg viewBox="0 0 1440 829" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <g id="banner" fill="var(--${colour}-${shade})" stroke="none" stroke-width="1" fill-rule="evenodd">
           <path d="M0,0 L1440,0 L1440,578.048796 C1292.5659,667.712567 1101.23257,738.423463 866,790.181484 C630.767434,841.939505 342.100767,841.939505 0,790.181484 L0,0 Z" id="Path"></path>
         </g>
       </svg>
     `
   }
-}
 
-function BannerSVG(props) {
-  return <span dangerouslySetInnerHTML={svgCode(props.id)} />
+  return <div className="svg-container"
+              id={id}
+              style={style}
+              dangerouslySetInnerHTML={svg} />
 }
 
 export default BannerSVG;
+
+
+// function BannerSVG({ data }) {
+//   const style = {
+//     backgroundImage: `url(${data})`,
+//     width: `100%`,
+//     height: `700px`,
+//     bacgroundColor: `red`,
+//   }
+
+//   return <div className="banner-svg" style={style} />
+// }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,30 @@
 // Librairies
 import React from 'react'
 import { Link, graphql } from 'gatsby'
-import Img from "gatsby-image"
+// import Img from "gatsby-image"
 
 // Components
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import BannerSVG from '../components/banner-svg';
 
+// CSS
+import '../css/pages/home.css'
+
+// SVG
+import bannerSVG from '../images/banner.svg';
+
 // Data
 import data from '../data/home-page';
 
 function HomePage(props) {
+  // console.log(bannerSVG);
   return (
     <Layout>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-      <BannerSVG id="red" />
+      <BannerSVG top="0" zIndex="-1" colour="yellow" shade="30" />
+      <BannerSVG top="35px" zIndex="-2" colour="yellow" shade="40" />
+      <BannerSVG top="70px" zIndex="-3" colour="yellow" shade="50" />
       <div className="container">
         <h1>
           {data.landing.value1}
@@ -23,6 +32,36 @@ function HomePage(props) {
         </h1>
         <h2>{data.landing.description}</h2>
       </div>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
     </Layout>
   )
 }
@@ -30,24 +69,24 @@ function HomePage(props) {
 export default HomePage;
 
 // This page query will be availabe in the props obj under data
-export const query = graphql`
-  query homePageQuery {
-    gatsbyIcon: file(name: {eq: "gatsby-icon"}) {
-      childImageSharp {
-        fluid {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    astronaut: file(name: {eq: "gatsby-astronaut"}) {
-      childImageSharp {
-        fixed(width: 125, height: 125) {
-          ...GatsbyImageSharpFixed
-        }
-      }
-    }
-  }
-`
+// export const query = graphql`
+//   query homePageQuery {
+//     gatsbyIcon: file(name: {eq: "gatsby-icon"}) {
+//       childImageSharp {
+//         fluid {
+//           ...GatsbyImageSharpFluid
+//         }
+//       }
+//     }
+//     astronaut: file(name: {eq: "gatsby-astronaut"}) {
+//       childImageSharp {
+//         fixed(width: 125, height: 125) {
+//           ...GatsbyImageSharpFixed
+//         }
+//       }
+//     }
+//   }
+// `
 
 
 // {
