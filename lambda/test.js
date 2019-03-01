1e0cc474db25ad39dce4d35059f6778dd50f25fe
Charly <charly.martin.1207@gmail.com>
2019-03-01 15:02:54 +0700

    Netlify functions setup

---
+++ lambda/test.js
@@ -0,0 +1,14 @@
+// Dev: netlify-lambda serve ./lambda
+// Prod: netlify-lambda build ./lambda
+
+// http://localhost:9000/test will run test.js
+exports.handler = function(event, context, callback) {
+  // your server-side functionality
+  // Netlify provides the event and context parameters when the function is invoked.
+  // You provide the callback parameter, which is optional, but recommended.
+  callback(null, {
+    statusCode: 200,
+    body: "Hello, World"
+    });
+}
+
