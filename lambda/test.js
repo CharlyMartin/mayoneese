ea77658809e733079e08bd1d5f30472e8adced6c
Charly <charly.martin.1207@gmail.com>
2019-03-01 15:10:29 +0700

    lambda set up

--- lambda/test.js
+++ lambda/test.js
@@ -3,6 +3,8 @@
 
 // http://localhost:9000/test will run test.js
 exports.handler = function(event, context, callback) {
+  console.log('event', event);
+  console.log('context', context.clientContext);
   // your server-side functionality
   // Netlify provides the event and context parameters when the function is invoked.
   // You provide the callback parameter, which is optional, but recommended.
