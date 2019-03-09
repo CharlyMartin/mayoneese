4508cd3a5ad44de4a3d346074f72ec5184024afb
Charly <charly.martin.1207@gmail.com>
2019-03-09 12:59:38 +0700

    rendering just once

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,73 +1,28 @@
 // import Librairies
-import React from 'react';
+import React from 'react'
 
 // Import CSS
-import './banner.css';
+import './banner.css'
 
 class Banner extends React.Component {
   constructor(props) {
     super(props)
-    // this.state = {
-    //   shadows: '',
-    //   shouldUpdate: true,
-    // }
   }
-
-  // buildCSS(colour, shade) {
-  //   return `var(--${colour}-${shade})`
-  // }
-
-  // renderStyle(colour, shade) {
-  //   // find a way to call renderStyle recursively
-  //   // until the shadows object is done enumerating
-  //   return {
-  //     background: this.buildCSS(colour, shade),
-  //     boxShadow: this.state.shadows
-  //   }
-  // }
-
-  triggerRainbow() {
-    // console.log(this.intervalID);
-    // clearInterval(this.intervalID);
-
-    // const shadows = [
-    //   ['yellow', 40],
-    //   ['yellow', 50],
-    //   ['orange', 40],
-    //   // ['orange', 50]
-    // ];
-    // for (let i = 0; i < shadows.length; i += 1) {
-    //   const [colour, shade] = shadows[i];
-    //   setTimeout(() => {
-    //     this.setState({
-    //       shadows: this.state.shadows + `0 2px 0 ${this.buildCSS(colour, shade)},`
-    //     })
-    //   }, i * 2000);
-    // }
-  }
-
-  // addBoxShadow() {
-  //   const shadow = '0 4px 0 var(--yellow-30)'
-  //   const newShadow = this.setState.shadows += shadow;
-  //   this.setState({shadows: newShadow});
-  //   // console.log(this);
-  // }
 
   shouldComponentUpdate() {
-    if (this.props.shadows === undefined) return false;
-    return true;
+    if (this.props.shadows === undefined) return false
+    return true
   }
 
-  componentDidUpdate() {
-    // Not possible to call setState here, it creates an infinite loops of renders.
-    console.log('updating');
-  }
+  // componentDidUpdate() {
+  //   // Not possible to call setState here, it creates an infinite loops of renders.
+  //   console.log('updating');
+  // }
 
   render() {
-    console.log('rendering');
-    const {colour, shade, position, children, shadows} = this.props;
-    // const shadowsString = shadows.join(', ');
-    // console.log(shadowsString);
+    console.log('rendering')
+    const { colour, shade, position, children, shadows } = this.props
+
     const style = {
       background: `var(--${colour}-${shade})`,
       boxShadow: shadows,
@@ -77,8 +32,8 @@ class Banner extends React.Component {
       <div className={`banner ${position}`} style={style}>
         {children}
       </div>
-    );
+    )
   }
 }
 
-export default Banner;
+export default Banner
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,32 +4,33 @@ import React from 'react'
 // import Img from "gatsby-image"
 
 // Components
-import Layout from '../components/layout';
-import SEO from '../components/seo';
-import Banner from '../components/banner';
-import MailchimpForm from '../components/mailchimp-form';
-import A from '../components/a';
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import Banner from '../components/banner'
+import MailchimpForm from '../components/mailchimp-form'
+import A from '../components/a'
 import H2Blob from '../components/h2'
 
 // CSS
-import './index.css';
+import './index.css'
 
 // Data
-import data from '../data/home';
+import data from '../data/home'
 
 class HomePage extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      bannerShadows: []
+      bannerShadows: [],
+      shadowRendered: false
     }
 
     this.handleButtonHover = this.handleButtonHover.bind(this);
   }
 
   buildShadow(height, colour, shade) {
-    return `0 ${height}px 0 var(--${colour}-${shade})`
+    return `0 ${height}px 0 var(--${colour}-${shade})`;
   }
 
   triggerShadows() {
@@ -43,35 +44,43 @@ class HomePage extends React.Component {
       ['red', 50],
       ['purple', 40],
       ['purple', 50],
-    ];
+    ]
 
     for (let i = 0; i < shadows.length; i++) {
-      const [colour, shade] = shadows[i];
+      const [colour, shade] = shadows[i]
 
       setTimeout(() => {
-        console.log(colour, shade);
-        const shadows = [...this.state.bannerShadows];
-        shadows.push(`${this.buildShadow(i * 25, colour, shade)}`);
+        console.log(colour, shade)
+        const shadows = [...this.state.bannerShadows]
+        shadows.push(`${this.buildShadow(i * 20, colour, shade)}`)
 
         this.setState({
-          bannerShadows: shadows
-        });
-      }, (i * 60));
+          bannerShadows: shadows,
+        })
+      }, i * 70)
     }
   }
 
   handleButtonHover(event) {
-    console.log(event);
-    this.triggerShadows();
+    // console.log(event)
+    if (!this.state.shadowRendered) {
+      this.triggerShadows();
+    }
+    this.setState({shadowRendered: true});
   }
 
   render() {
     return (
       <Layout>
         <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-        
+
         <div id="home-page">
-          <Banner colour="yellow" shade="30" position="bottom" shadows={this.state.bannerShadows}>
+          <Banner
+            colour="yellow"
+            shade="30"
+            position="bottom"
+            shadows={this.state.bannerShadows}
+          >
             <section id="hero">
               <div className="container">
                 <div className="hero-content">
@@ -80,73 +89,75 @@ class HomePage extends React.Component {
                     <span className="title">{data.landing.value2}</span>
                   </h1>
                   <h3>{data.landing.description}</h3>
-                
+
                   <MailchimpForm hoverCallback={this.handleButtonHover} />
-                  <p className="description text-small">{data.form.description}</p>
+                  <p className="description text-small">
+                    {data.form.description}
+                  </p>
                 </div>
               </div>
             </section>
           </Banner>
-  
-          
+
           <div className="features">
-  
             <section id="problem">
               <div className="container-sm">
                 <div className="text-left">
-  
-                  <H2Blob colour="purple" position="right">{data.problem.title}</H2Blob>
-  
+                  <H2Blob colour="purple" position="right">
+                    {data.problem.title}
+                  </H2Blob>
+
                   <span className="first-patagraph">
                     <p>{data.problem.text1}</p>
-                    <A href={data.problem.link}>
-                      {data.problem.linkText}
-                    </A>
+                    <A href={data.problem.link}>{data.problem.linkText}</A>
                   </span>
-  
+
                   <p>{data.problem.text2}</p>
                   <p className="solution">{data.problem.solution}</p>
-  
                 </div>
               </div>
             </section>
-  
+
             <section id="why">
               <div className="container-sm">
                 <div className="text-right">
-                  <H2Blob colour="red" position="left">{data.why.title}</H2Blob>
+                  <H2Blob colour="red" position="left">
+                    {data.why.title}
+                  </H2Blob>
                   <p>{data.why.text}</p>
                 </div>
               </div>
             </section>
-  
+
             <section id="how">
               <div className="container-sm">
                 <div className="text-left">
-                  <H2Blob colour="orange" position="right">{data.how.title}</H2Blob>
+                  <H2Blob colour="orange" position="right">
+                    {data.how.title}
+                  </H2Blob>
                   <p>{data.how.text}</p>
                 </div>
               </div>
             </section>
-  
+
             <section id="what">
               <div className="container-sm">
                 <div className="text-right">
-                  <H2Blob colour="yellow" position="left">{data.what.title}</H2Blob>
+                  <H2Blob colour="yellow" position="left">
+                    {data.what.title}
+                  </H2Blob>
                   <p>{data.what.text}</p>
                 </div>
               </div>
             </section>
-            
           </div>
-          
         </div>
       </Layout>
     )
   }
 }
 
-export default HomePage;
+export default HomePage
 
 // This page query will be availabe in the props obj under data
 // export const query = graphql`
@@ -160,7 +171,6 @@ export default HomePage;
 //     }
 //   }
 // `
-
 
 // {
 //   allFile(filter: {sourceInstanceName: {eq: "images"}}) {
