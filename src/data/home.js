484346eb04760f2ba9e5938f06835640e4656574
Charly <charly.martin.1207@gmail.com>
2019-03-13 11:07:56 +0700

    new content

--- src/data/home.js
+++ src/data/home.js
@@ -2,33 +2,39 @@ export default {
   landing: {
     value1: 'Music that makes you',
     value2: 'come alive',
-    description: "Get awesome music tracks in your inbox every week. Your life is about to be brighter. You're welcome.",
+    description:
+      "Get awesome music tracks in your inbox every week. Your life is about to be brighter. You're welcome.",
   },
   form: {
-    description: 'Sign up! The music sounds better with you.'
+    description: 'Sign up! The music sounds better with you.',
   },
   problem: {
     title: 'Mainstream music is… bleh',
-    text1: 'Listening to music from mainstream media is boring. It’s the same overplayed songs that all sound similar, when they are not exactly the same. ',
-    text2: 'Most of us don’t know where to look for alternatives, or don’t have time for it. So we default to what is served to us and change tunes like we swipe Tinder profiles. We know the feeling.',
+    text1:
+      'Listening to music from mainstream media is boring. It’s the same overplayed songs that all sound similar, when they are not exactly the same. ',
+    text2:
+      'Most of us don’t know where to look for alternatives, or don’t have time for it. So we default to what is served to us and change tunes like we swipe Tinder profiles. We know the feeling.',
     linkText: 'See for yourself.',
     link: 'https://www.youtube.com/watch?v=7HWn9NGgQks',
     solution: 'That’s where Mayoneese comes to the rescue 🔥',
   },
   why: {
     title: 'Good music makes your life better',
-    text: 'At Mayoneese, we believe that listening to great music everyday can make your days brighter. So we dig out the web and bring you our best findings. Is this really a job?'
+    text:
+      'At Mayoneese, we believe that listening to great music everyday can make your days brighter. So we dig out the web and bring you our best findings. Is this really a job?',
   },
   how: {
     title: 'We want to make you feel',
-    text: 'We’re looking for one thing: music that makes you smile, dance, move, jump or want to bang your head wherever you deem appropriate. You remember that feeling of listening to a great track for the first time? Imagine that every week.'
+    text:
+      'We’re looking for one thing: music that makes you smile, dance, move, jump or want to bang your head wherever you deem appropriate. You remember that feeling of listening to a great track for the first time? Imagine that every week.',
   },
   what: {
-    title: 'House, Disco & Electro',
-    text: 'We send you the best of disco, house and electronic music every week: hidden gems that deserve more attention and the old forgotten tracks that still work. La creme de la creme.'
+    title: 'La creme de la creme',
+    text:
+      'We send you the best of the genres we love: Disco, House, Electro, Pop... The hidden gems that deserve more attention and the forgotten tracks that still work. Only the good stuff.',
   },
   footer: {
-    title: "Join mayoneese now!",
-    description: "And be that cool person who plays great music at parties."
-  }
+    title: 'Join mayoneese now!',
+    description: 'And be that cool person who plays great music at parties.',
+  },
 }
